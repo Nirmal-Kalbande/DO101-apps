7514b56997359f8f01f33dbb22412c024f9886a4
NK_21 <kalbandenirmal4@gmail.com>
2025-02-02 20:24:03 +0530

    Update"tookplace"app.js

--- weather/app.js
+++ weather/app.js
@@ -33,7 +33,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error took place');
 });
 
 module.exports = app;
